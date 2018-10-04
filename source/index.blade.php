300844070c8bd0743b4a38f2c770fa3cf180b70e
Tanner Bryant <tanner@nobleintegrationsllc.com>
2018-10-04 14:14:12 -0500

    Layout 2 Fullscreen done

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,4 +3,7 @@
 @section('body')
 <h1>Hello world!</h1>
 <a href="/layout1">Layout 1</a>
+<a href="/layout1-fullscreen">Layout 1 Fullscreen</a>
+<a href="/layout2">Layout 2</a>
+<a href="/layout3">Layout 3</a>
 @endsection
