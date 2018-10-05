4709f5082ab184f344267af13b311672f03c9132
Tanner Bryant <tanner@nobleintegrationsllc.com>
2018-10-05 09:17:13 -0500

    Removed extra CSS with purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,14 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
 var tailwindcss = require('tailwindcss');
+let glob = require("glob-all");
+let PurgecssPlugin = require("purgecss-webpack-plugin");
+
+class TailwindExtractor {
+    static extract(content) {
+        return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+    }
+}
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
@@ -18,3 +26,31 @@ mix.js('source/_assets/js/main.js', 'js')
         processCssUrls: false,
         postCss: [tailwindcss('tailwind.js')],
     }).version();
+
+// Only run PurgeCSS during production builds for faster development builds
+// and so you still have the full set of utilities available during
+// development.
+if (mix.inProduction()) {
+    console.log('production mode - engaged')
+    mix.webpackConfig({
+        plugins: [
+            new PurgecssPlugin({
+
+                // Specify the locations of any files you want to scan for class names.
+                paths: glob.sync([
+                    path.join(__dirname, "source/**/*.blade.php"),
+                    //path.join(__dirname, "source/**/js/**/*.vue")
+                ]),
+                extractors: [
+                    {
+                        extractor: TailwindExtractor,
+
+                        // Specify the file extensions to include when scanning for
+                        // class names.
+                        extensions: ["html", "js", "php", "vue"]
+                    }
+                ]
+            })
+        ]
+    });
+}
